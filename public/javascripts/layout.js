4b8d8ad95478f355cdc208df6d601367c8151055
psx980626 <375180728@qq.com>
2017-08-03 22:43:15 +0800

    倒计时修改

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -1,47 +1,32 @@
-// 倒计时
+//计算剩余时间
+var days,
+    hours,
+    minutes,
+    seconds;
 
-// function $$(a) {
-//     return document.querySelector(a);
-// }
+function leftTimer(year, mon, days, hours, minutes, seconds) {
+    var leftTime = (new Date(year, mon - 1, days, hours, minutes, seconds)) - (new Date());
+        days = parseInt(leftTime / 1000 / 60 / 60 / 24, 10);
+        hours = parseInt(leftTime / 1000 / 60 / 60 % 24, 10);
+        minutes = parseInt(leftTime / 1000 / 60 % 60, 10);
+        seconds = parseInt(leftTime / 1000 % 60, 10);
+        showTime(days, 'day');
+        showTime(hours,'hours');
+        showTime(minutes,'min');
+        showTime(seconds,'sec');
+}
 
-// var day = $$('.day');
-// var hour = $$('.hours');
-// var min = $$('.min');
-// var sec = $$('.sec');
+//用图标显示剩余时间
+function showTime(a, div) {
+    var list = [2.3, 11.7, 21.05, 30.4, 39.8, 49, 58.4, 67.6, 77, 86.4];
+    Tens = parseInt(a / 10);
+    Single = a % 10;
+    yT = list[Tens];
+    yS = list[Single];
+    console.log(yT);
+    $( '.' + div + 'T').css('backgroundPosition', "40%" + yT +"%");
+    $('.' + div + 'S').css('backgroundPosition', "40%" + yS +"%");
+}
 
+window.setInterval("leftTimer(2017,9,14,0,0,0)", 1000);
 
-// //计算剩余时间
-// function leftTimer (year,mon,days,hours,minutes,seconds){
-//     var leftTime = (new Date(year, mon - 1, days, hours, minutes, seconds)) - (new Date());
-//     var days = parseInt(leftTime / 1000 / 60 / 60 / 24 , 10),
-//         hours = parseInt(leftTime / 1000 / 60 / 60 % 24 , 10), 
-//         minutes = parseInt(leftTime / 1000 / 60 % 60, 10),
-//         seconds = parseInt(leftTime / 1000 % 60, 10);
-//     showTime(days);
-//     day.innerHTML = _f + _s;
-//     showTime(hours);
-//     hour.innerHTML = _f + _s;
-//     showTime(minutes);
-//     min.innerHTML = _f + _s;
-//     showTime(seconds);
-//     sec.innerHTML = _f + _s;
-// }
-
-// //用图标显示剩余时间
-// function showTime(a){
-//     Tens = parseInt(a / 10);
-//     Single = a % 10;
-//     _f = "<img src='./images/" + Tens + ".png'>";
-//     _s = "<img src='./images/" + Single + ".png'>";
-// }
-
-// window.setInterval("leftTimer(2017,9,14,0,0,0)",1000);
-
-
-// 导航栏特效
-
-
-// $(document).ready(function () {
-//     let pathName = location.pathname;
-//     switch 
-// })
