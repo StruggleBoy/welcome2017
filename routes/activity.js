d79356af4cae4ff236933f1774fbd4415d69eabe
Messiahhh <616348105@qq.com>
2017-08-10 02:40:47 +0800

    update:实现学长学姐帮帮忙基本功能

--- routes/activity.js
+++ routes/activity.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var request = require('request')
+
 
 router.get('/', function(req, res, next) {
-    res.render('activity');
+    request('http://hongyan.cqupt.edu.cn/XZBBM/index.php/api/search/tag/', function (err, response, body) {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            let data = JSON.parse(body)
+            let len = Math.floor(data.length / 11) + 1
+            res.render('activity', {data: data, len: len});
+        }
+    })
+    
 });
 
 module.exports = router;
